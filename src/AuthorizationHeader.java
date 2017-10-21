f8051f731c7b45e752cb6d0c8633f0bafc3da3f7
jeff457 <jstanton397@yahoo.com>
2017-10-21 14:13:48 -0700

    add docusign class

---
+++ src/AuthorizationHeader.java
@@ -0,0 +1,15 @@
+
+public class AuthorizationHeader 
+{
+	public static final AuthorizationHeader INSTANCE = new AuthorizationHeader();
+	
+	private AuthorizationHeader() {}
+	
+	private final String Username = "\"jstanton397@gmail.com\"";
+	private final String Password = "\"dfui4016\"";
+	private final String IntegratorKey = "\"1c01c227-f26b-4d86-b194-f8fd20171a99\"";
+	
+	public String toString() {
+		return "{ Username: " + Username + ", Password: " + Password + ", IntegratorKey: " + IntegratorKey + "}";
+	}
+}
